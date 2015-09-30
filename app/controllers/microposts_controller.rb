5d7339893d55ca072fea29fe3f726f62a3d6771b
Cies Breijs <cies@kde.nl>
2015-09-30 09:36:54 +0200

    Update microposts_controller.rb

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -17,10 +17,11 @@ class MicropostsController < ApplicationController
     redirect_to root_path, :flash => { :success => "Micropost deleted!" }
   end
   
+  
   private
   
-    def authorized_user
-      @micropost = current_user.microposts.find_by_id(params[:id])
-      redirect_to root_path if @micropost.nil?
-    end
+  def authorized_user
+    @micropost = current_user.microposts.find_by_id(params[:id])
+    redirect_to root_path if @micropost.nil?
+  end
 end
